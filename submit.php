88d088427b68802a42f87d36879cfff0a60f6070
Joris van rijn <jorisvanrijn2006@gmail.com>
2024-03-15 10:03:13 +0100

    added character limit on name and message
    message doesn't post when exceeded

--- submit.php
+++ submit.php
@@ -1,23 +1,25 @@
 <?php
 $visit = 1;
-$myjsoncontent = file_get_contents("json/messages.json");
-if (isset($_POST["message"]) && $_POST["message"] == "") {
-    $visit = 0;
-} else if (isset($_POST["message"])) {
-    $visit = 5;
-    if (isset($_POST["submit"]) && $_POST["submit"] == "Send") {
-        $msg = new stdClass();
-        $myjsoncontent = file_get_contents("json/messages.json");
-        $messages = json_decode($myjsoncontent, true);
+$characterlimitmessage = 1000;
+$characterlimitname = 255;
+if (strlen($_POST['massage']) < $characterlimitmessage || strlen($_POST['name']) < $characterlimitname) {
+    $myjsoncontent = file_get_contents("json/messages.json");
+    if (isset ($_POST["message"]) && $_POST["message"] == "") {
+        $visit = 0;
+    } else if (isset ($_POST["message"])) {
+        $visit = 5;
+        if (isset ($_POST["submit"]) && $_POST["submit"] == "Send") {
+            $msg = new stdClass();
+            $myjsoncontent = file_get_contents("json/messages.json");
+            $messages = json_decode($myjsoncontent, true);
 
-        if ($messages === null) {
-            $messages = [];
-        }
-
-        // Check if id already exists
-        $existingIds = array_column($messages, 'id');
-        $existingMessages = array_column($messages, 'message');
-        $existingnames = array_column($messages, 'name');
+            if ($messages === null) {
+                $messages = [];
+            }
+            // Check if id already exists
+            $existingIds = array_column($messages, 'id');
+            $existingMessages = array_column($messages, 'message');
+            $existingnames = array_column($messages, 'name');
             $visit = 6;
             $lastId = end($messages)['id'] ?? 0;
             $msg->name = $_POST["name"];
@@ -25,7 +27,7 @@ if (isset($_POST["message"]) && $_POST["message"] == "") {
             $msg->time = date("h:i:s");
             $msg->id = $lastId + 1;
 
-            if (!empty($_FILES['fileToUpload']['name'])) {
+            if (!empty ($_FILES['fileToUpload']['name'])) {
                 $target_dir = "uploads/";
                 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
                 $uploadOk = 1;
@@ -73,17 +75,17 @@ if (isset($_POST["message"]) && $_POST["message"] == "") {
                         echo "ERROR 7012: Sorry, therE waS an error uploading your file.";
                     }
                 }
+            } else {
+                echo "what is an image";
             }
-            else{
-                echo"what is an image";
-            }
-            
+
             $messages[] = $msg;
 
             $updatedJsonContent = json_encode($messages);
             file_put_contents("json/messages.json", $updatedJsonContent);
         }
     }
+}
 
 header("Location: index.php", true, 301);
 exit();
